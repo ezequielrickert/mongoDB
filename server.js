2b8c41e5a6f03369074544d36e29fa275b41dcf3
ezequielrickert <erickert@mail.austral.edu.ar>
2024-05-27 10:31:10 -0300

    Changes by Elías

--- server.js
+++ server.js
@@ -10,66 +10,13 @@ const mqttClient = mqtt.connect(mqttUri);
 const mongoUri = 'mongodb://' + config.mongodb.hostname + ':' + config.mongodb.port;
 const mongoClient = new MongoClient(mongoUri);
 
-mqttClient.on("connect", () => {
-  subscribeToTopic("/login");
-  console.log("MQTT Client connected");
-});
-mqttClient.on("message", async (topic, message) => {
-  if (message != null) {
-    message = JSON.parse(String(message));
-    console.log(message);
-    switch (topic) {
-      case '/login':
-        await handleLogin(message);
-        break;
-        // Add more cases as needed for other topics
-      default:
-        console.log(`No handler for topic ${topic}`);
-    }
-  };
-});
+// Import controllers
+const LoginController = require('/controller/LoginController');
+const RegisterController = require('controller/RegisterController');
 
-async function handleLogin(message) {
-  try {
-    await mongoClient.connect();
-    const database = mongoClient.db(config.mongodb.database);
-    const users = database.collection("users");
+// Create controller instances
+const loginController = new LoginController(mqttClient, mongoClient, config);
+const registerController = new RegisterController(mqttClient, mongoClient, config);
 
-    // Assuming message contains a 'username' property
-    const user = await users.findOne({ username: message.username });
-    if (user) {
-      // User exists, publish user_Id and house_Id
-      mqttClient.publish('/user_data', JSON.stringify({ userId: user._id }));
-    } else {
-      console.log('User does not exist');
-    }
-  } finally {
-    await mongoClient.close();
-  }
-}
-
-function subscribeToTopic(topic) {
-  mqttClient.subscribe(topic, (err) => {
-    if (!err) {
-      console.log(`Subscribed to topic: ${topic}`);
-    } else {
-      console.error(`Failed to subscribe to topic: ${topic}`);
-    }
-  });
-}
-
-
-async function run(msg) {
-  try {
-    await mongoClient.connect();
-    const database = mongoClient.db(config.mongodb.database);
-    const haiku = database.collection("message");
-
-    const date_time = new Date();
-
-    const result = await haiku.insertOne(JSON.parse(msg));
-    console.log(`A document was inserted with the _id: ${result.insertedId}`);
-  } finally {
-    await mongoClient.close();
-  }
-}
+// Controllers are now listening for their respective topics
+console.log("Application started");
