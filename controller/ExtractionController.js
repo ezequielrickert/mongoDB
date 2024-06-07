a130ce7a17fab799bea7a9ed74156b465ec987a2
ezequielrickert <erickert@mail.austral.edu.ar>
2024-06-07 16:14:07 -0300

    updated Block.jsx and ExtractionController.js

--- controller/ExtractionController.js
+++ controller/ExtractionController.js
@@ -23,12 +23,15 @@ class ExtractionController extends BaseController {
                 if (extractionLimit >= jsonMessage.amount) {
                     await houses.updateOne({houseId: jsonMessage.houseId}, {$inc: {amount: -amount}});
                     console.log(`Extracted ${jsonMessage.amount} from the safebox. New limit is ${house.amount - jsonMessage.amount}`);
+                    this.publish("/extraction_confirmation", {message: "Extraction completed"});
                 }
                 else {
                     console.log('You are over the extraction limit');
+                    this.publish("/extraction_confirmation", {message: "You are over the extraction limit"});
                 }
             } else {
                 console.log("House does not exist");
+                this.publish("/extraction_confirmation", {message: "House does not exist"});
             }
         } finally {
             await this.mongoClient.close();
