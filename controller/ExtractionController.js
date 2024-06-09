698eadf26fc5b78299f8e5b100facf29f8406c7a
ezequielrickert <erickert@mail.austral.edu.ar>
2024-06-09 15:17:20 -0300

    new changes

--- controller/ExtractionController.js
+++ controller/ExtractionController.js
@@ -12,22 +12,36 @@ class ExtractionController extends BaseController {
             await this.mongoClient.connect();
             const database = this.mongoClient.db(this.config.mongodb.database);
             const houses = database.collection("houses");
-            const jsonMessage = JSON.parse(JSON.parse(String(message)));
+            const users = database.collection("users");
+            const jsonMessage = JSON.parse(String(message));
 
             const house = await houses.findOne({ houseId: jsonMessage.houseId });
+
+            const user = await users.findOne({ _id: jsonMessage.userId});
 
             const extractionLimit = house.limit === 0 ? house.amount : house.limit;
 
             if (house) {
-                const amount = Number(jsonMessage.amount);
-                if (extractionLimit >= jsonMessage.amount) {
-                    await houses.updateOne({houseId: jsonMessage.houseId}, {$inc: {amount: -amount}});
-                    console.log(`Extracted ${jsonMessage.amount} from the safebox. New limit is ${house.amount - jsonMessage.amount}`);
-                    this.publish("/extraction_confirmation", {message: "Extraction completed"});
+                if (user) {
+                    if (!user.isBlocked) {
+                        const amount = Number(jsonMessage.amount);
+                        if (extractionLimit >= jsonMessage.amount) {
+                            await houses.updateOne({houseId: jsonMessage.houseId}, {$inc: {amount: -amount}});
+                            console.log(`Extracted ${jsonMessage.amount} from the safebox. New limit is ${house.amount - jsonMessage.amount}`);
+                            this.publish("/extraction_confirmation", {message: "Extraction completed"});
+                        } else {
+                            console.log('You are over the extraction limit');
+                            this.publish("/extraction_confirmation", {message: "You are over the extraction limit"});
+                        }
+                    }
+                    else {
+                        console.log("You are not allowed to extract money");
+                        this.publish("/extraction_confirmation", {message: "You are not allowed to extract money"});
+                    }
                 }
                 else {
-                    console.log('You are over the extraction limit');
-                    this.publish("/extraction_confirmation", {message: "You are over the extraction limit"});
+                    console.log("User not found");
+                    this.publish(("/extraction_confirmation", {message: "User not found"}));
                 }
             } else {
                 console.log("House does not exist");
